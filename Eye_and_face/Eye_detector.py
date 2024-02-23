d25147bbf68ff28a010ba87c1c36059545808265
LorisGiordano <152272505+LorisGiordano@users.noreply.github.com>
2024-02-23 12:36:56 +0100

    Add files via upload

---
+++ Eye_and_face/Eye_detector.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 17 09:11:59 2021
+
+@author: bkeelson
+"""
+import cv2
+
+# Load the cascade
+cascade_dir = r"/Users/loris/anaconda3/envs/EIT_project/lib/python3.11/site-packages/cv2/data"
+eye_cascade = cv2.CascadeClassifier(cascade_dir + r"/haarcascade_eye.xml")
+face_cascade = cv2.CascadeClassifier(cascade_dir + r"/haarcascade_frontalface_default.xml")
+
+
+# To capture video from webcam.
+cap = cv2.VideoCapture(0)
+# To use a video file as input
+# cap = cv2.VideoCapture('filename.mp4')
+
+cat_filename = __file__.split('.')[0] + "_Cat.png"
+cat_img = cv2.imread(cat_filename, cv2.IMREAD_COLOR)
+  
+cat_img = cv2.resize(cat_img, (200,200))
+
+while True:
+    # Read the frame
+    _, img = cap.read()
+
+    # Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    for (x, y, w, h) in face_cascade.detectMultiScale(gray, 1.1, 4):
+        img[y:y + h, x:x + w] = cv2.resize(cat_img, (h, w))
+
+    # Draw the rectangle around each face
+    # for (x, y, w, h) in eye_cascade.detectMultiScale(gray, 1.1, 4):
+        # img[y:y+h, x:x+w] = cv2.resize(cat_img, (h,w))
+
+    # Display
+    cv2.namedWindow("img", cv2.WINDOW_FREERATIO)
+    cv2.imshow('img', img)
+    # Stop if escape key is pressed
+    k = cv2.waitKey(30) & 0xff
+    if k==27:
+        break
+    
+# Release the VideoCapture object
+cap.release()
+cv2.destroyAllWindows()
---
+++ People/People_detector.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 26 09:46:11 2021
+
+@author: bkeelson
+"""
+# import the necessary packages
+import numpy as np
+import cv2
+
+webcam = True
+
+# open webcam video stream
+if webcam:
+    cap = cv2.VideoCapture(0)
+    
+else:
+    import pyrealsense2 as rs
+    # Configure streams
+    pipeline = rs.pipeline()
+    config = rs.config()
+
+    config.enable_stream(rs.stream.depth, rs.format.z16, 30)
+    other_stream, other_format = rs.stream.color, rs.format.bgr8
+    config.enable_stream(other_stream, other_format, 30)
+
+    # Start streaming
+    pipeline.start(config)
+    profile = pipeline.get_active_profile()
+
+
+# initialize the HOG descriptor/person detector
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+cv2.startWindowThread()
+
+# the output will be written to output.avi
+out = cv2.VideoWriter(
+    'output.avi',
+    cv2.VideoWriter_fourcc(*'MJPG'),
+    15.,
+    (640,480))
+
+try:
+    while(True):
+        if webcam:
+            # Capture frame-by-frame for computer webcam
+            ret, frame = cap.read()
+        
+        else:
+            frames = pipeline.wait_for_frames()
+            color_frame = frames.get_color_frame()
+            color_image = np.asanyarray(color_frame.get_data())
+                
+        
+            # resizing for faster detection
+            frame = cv2.resize(color_image, (640, 480))
+        
+        # using a greyscale picture, also for faster detection
+        gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    
+        # detect people in the image
+        # returns the bounding boxes for the detected objects
+        boxes, weights = hog.detectMultiScale(gray, winStride=(8,8) )
+    
+        boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
+    
+        for (xA, yA, xB, yB) in boxes:
+            # display the detected boxes in the colour picture
+            cv2.rectangle(frame, (xA, yA), (xB, yB),
+                              (0, 255, 0), 2)
+        
+        # Write the output video 
+        out.write(frame.astype('uint8'))
+        # Display the resulting frame
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        
+finally:
+    if webcam:
+        # When everything done, release the capture
+        cap.release()
+    else:
+        pipeline.stop()
+    # release the output
+    out.release()
+    # finally, close the window
+    cv2.destroyAllWindows()
