647ca924587dbfff161cef3be0bb58daeef9a51d
LorisGiordano <152272505+LorisGiordano@users.noreply.github.com>
2025-01-28 13:16:56 +0100

    Update People_detector.py

--- Detection/People/People_detector.py
+++ Detection/People/People_detector.py
@@ -76,7 +76,10 @@ if __name__ == '__main__':
             out.write(frame.astype('uint8'))
             # Display the resulting frame
             cv2.imshow('frame',frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
+            
+            # Escape
+            k = cv2.waitKey(1)
+            if k == 27:
                 break
             
     finally:
