52a1c0535afd7b16f5f5da253dc3ff0dccba25af
LorisGiordano <152272505+LorisGiordano@users.noreply.github.com>
2025-02-14 12:39:39 +0100

    Update DepthTracker.py

--- Detection/Depth/DepthTracker.py
+++ Detection/Depth/DepthTracker.py
@@ -140,8 +140,12 @@ if __name__ == '__main__':
     align_to = rs.stream.color
     align = rs.align(align_to)
 
+    # Path to cv2/data. If this gives an error:
+    #   - option 1: Find the 'haarcascade_eye.xml' file on your computer and modify the path hereunder to the path of the folder containing 'haarcascade_eye.xml'.
+    #   - option 2: You can also download haarcascade_eye.xml from the internet, place it in your project folder, and modify the path hereunder.
+    cascade_dir = os.environ['CONDA_PREFIX'] + r"/lib/python3.12/site-packages/cv2/data"
+    
     # Load the cascade
-    cascade_dir = os.environ['CONDA_PREFIX'] +  r"/lib/python3.13/site-packages/cv2/data"
     face_cascade = cv2.CascadeClassifier(cascade_dir + r"/haarcascade_frontalface_default.xml")
 
     # List of post processing filters
