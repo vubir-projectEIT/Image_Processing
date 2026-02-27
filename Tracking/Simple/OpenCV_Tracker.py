08b077da2dc24bf0ba43322f5707fac02a781f53
Loris Giordano <loris.giordano@vub.be>
2026-02-27 10:27:33 +0100

    Fix OpenCV_Tracker version check

--- Tracking/Simple/OpenCV_Tracker.py
+++ Tracking/Simple/OpenCV_Tracker.py
@@ -3,12 +3,6 @@
 import cv2
 import sys
  
-ver = cv2.__version__
-minor_ver = ver[2]
-
-
-# (major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')￼
- 
  
 """ MAIN """
 
@@ -20,25 +14,22 @@ if __name__ == '__main__' :
     tracker_types = ['BOOSTING', 'MIL','KCF', 'TLD', 'MEDIANFLOW', 'MOSSE', 'CSRT'] # 'GOTURN' not in legacy
     tracker_type = 'CSRT'
  
-    if int(minor_ver) < 3:
-        tracker = cv2.Tracker_create(tracker_type)
-    else:
-        if tracker_type == 'BOOSTING':
-            tracker = cv2.legacy.TrackerBoosting_create()
-        if tracker_type == 'MIL':
-            tracker = cv2.legacy.TrackerMIL_create()
-        if tracker_type == 'KCF':
-            tracker = cv2.legacy.TrackerKCF_create()
-        if tracker_type == 'TLD':
-            tracker = cv2.legacy.TrackerTLD_create()
-        if tracker_type == 'MEDIANFLOW':
-            tracker = cv2.legacy.TrackerMedianFlow_create()
-        if tracker_type == 'GOTURN':
-            tracker = cv2.legacy.TrackerGOTURN_create()
-        if tracker_type == 'MOSSE':
-            tracker = cv2.legacy.TrackerMOSSE_create()
-        if tracker_type == "CSRT":
-            tracker = cv2.legacy.TrackerCSRT_create()
+    if tracker_type == 'BOOSTING':
+        tracker = cv2.legacy.TrackerBoosting_create()
+    if tracker_type == 'MIL':
+        tracker = cv2.legacy.TrackerMIL_create()
+    if tracker_type == 'KCF':
+        tracker = cv2.legacy.TrackerKCF_create()
+    if tracker_type == 'TLD':
+        tracker = cv2.legacy.TrackerTLD_create()
+    if tracker_type == 'MEDIANFLOW':
+        tracker = cv2.legacy.TrackerMedianFlow_create()
+    if tracker_type == 'GOTURN':
+        tracker = cv2.legacy.TrackerGOTURN_create()
+    if tracker_type == 'MOSSE':
+        tracker = cv2.legacy.TrackerMOSSE_create()
+    if tracker_type == "CSRT":
+        tracker = cv2.legacy.TrackerCSRT_create()
  
     # Read video
     #video = cv2.VideoCapture("videos/chaplin.mp4")
